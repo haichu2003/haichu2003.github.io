8c8cb6305d72984a1735ebea69a1f371ae128975
Chu Hai <duchaichu1303@gmail.com>
2023-02-12 00:59:04 +0200

    added files

--- js/new.js
+++ js/new.js
@@ -1,8 +1,31 @@
+// function menuButton() {
+// }
+
 function menuButton() {
-    if(document.getElementById("headerLink1").style.visibility == "hidden") document.getElementById("headerLink1").style.visibility = "visible";
+    if (document.getElementById("header").style.visibility == "hidden")
+        document.getElementById("header").style.visibility = "visible";
+    else document.getElementById("header").style.visibility = "hidden";
+    if (document.getElementById("headerLink1").style.visibility == "hidden")
+        document.getElementById("headerLink1").style.visibility = "visible";
     else document.getElementById("headerLink1").style.visibility = "hidden";
-    if(document.getElementById("headerLink2").style.visibility == "hidden") document.getElementById("headerLink2").style.visibility = "visible";
+    if (document.getElementById("headerLink2").style.visibility == "hidden")
+        document.getElementById("headerLink2").style.visibility = "visible";
     else document.getElementById("headerLink2").style.visibility = "hidden";
-    if (document.getElementById("headerLink3").style.visibility == "hidden") document.getElementById("headerLink3").style.visibility = "visible";
+    if (document.getElementById("headerLink3").style.visibility == "hidden")
+        document.getElementById("headerLink3").style.visibility = "visible";
     else document.getElementById("headerLink3").style.visibility = "hidden";
 }
+
+window.onscroll = function scrollFunction() {
+    let navbar = document.getElementById("navbar");
+    if (
+        document.body.scrollTop > 30 ||
+        document.documentElement.scrollTop > 30
+    ) {
+        navbar.style.display = "none";
+        navbar.style.maxHeight = null;
+    } else {
+        navbar.style.display = "block";
+        navbar.style.maxHeight = "44px";
+    }
+};
