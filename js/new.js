6c00b71b527bd634204b59042745ff3b10ccbfc1
Chu Hai <duchaichu1303@gmail.com>
2023-02-12 01:22:00 +0200

    added files

--- js/new.js
+++ js/new.js
@@ -18,12 +18,15 @@ function menuButton() {
 
 window.onscroll = function scrollFunction() {
     let navbar = document.getElementById("navbar");
+    var x, y;
     if (
         document.body.scrollTop > 30 ||
         document.documentElement.scrollTop > 30
     ) {
         navbar.style.display = "none";
         navbar.style.maxHeight = null;
+        x = document.body.scrollTop;
+        y = document.documentElement.scrollTop;
     } else {
         navbar.style.display = "block";
         navbar.style.maxHeight = "44px";
