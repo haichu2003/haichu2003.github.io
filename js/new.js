e73dbdedd42e8d3ba467557311ca3dc1634e2ac7
Chu Hai <duchaichu1303@gmail.com>
2023-02-12 00:03:47 +0200

    added files

---
+++ js/new.js
@@ -0,0 +1,8 @@
+function menuButton() {
+    if(document.getElementById("headerLink1").style.visibility == "hidden") document.getElementById("headerLink1").style.visibility = "visible";
+    else document.getElementById("headerLink1").style.visibility = "hidden";
+    if(document.getElementById("headerLink2").style.visibility == "hidden") document.getElementById("headerLink2").style.visibility = "visible";
+    else document.getElementById("headerLink2").style.visibility = "hidden";
+    if (document.getElementById("headerLink3").style.visibility == "hidden") document.getElementById("headerLink3").style.visibility = "visible";
+    else document.getElementById("headerLink3").style.visibility = "hidden";
+}
